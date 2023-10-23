87e5312f1d82b600dd03e695a4cc7e35b9b3c3f3
Jayden Deng <dqy676081195@gmail.com>
2023-10-23 18:14:47 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<username>.github.io/<repo>
+# 部署到 https://<username>.github.io
 git push -f git@github.com:KetherTahm/vitepress-kether-tahm.git master:github-pages
 
-# 提交所有代码到github
+## 提交所有代码到 github
 cd ../../../
 git add -A
 git commit -m 'update'
